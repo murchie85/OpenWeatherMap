c09cc1995a080d0dc5a99f3edbb59649761b0094
Adam <murchie85@gmail.com>
2018-08-27 23:40:22 +0100

    windspeed added

--- buildWeatherReport.py
+++ buildWeatherReport.py
@@ -19,6 +19,7 @@ for x in range(0, len(data)):
 	print("temp        : " + str(data[x]['main']['temp']))
 	print("temp_max    : " + str(data[x]['main']['temp_max']))
 	print("temp_min    : " + str(data[x]['main']['temp_min']))
+	print("Wind info   : " + str(data[x]['wind']))
 
 
 	print("End of City Report \n")
